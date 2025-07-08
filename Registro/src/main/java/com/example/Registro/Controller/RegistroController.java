e57fa6403f47cd74e43a6984e9d45e6efab980f0
Franar22 <fran.argandona@duocuc.cl>
2025-07-08 17:08:44 -0400

    Cambios de hateoas en registro y login

--- Registro/src/main/java/com/example/Registro/Controller/RegistroController.java
+++ Registro/src/main/java/com/example/Registro/Controller/RegistroController.java
@@ -8,33 +8,50 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/registro")
 public class RegistroController {
+
     @Autowired
     private RegistroService registroService;
-    
-    @Operation(summary = "Registrar un nuevo usuario",description = "Permite registrar un nuevo usuario con los datos necesarios. Devuelve el usuario registrado si es exitoso.")
+
+    @Operation(
+        summary = "Registrar un nuevo usuario",
+        description = "Permite registrar un nuevo usuario con los datos necesarios. Devuelve el usuario registrado si es exitoso."
+    )
     @ApiResponses(value = {
-    @ApiResponse(responseCode = "201", description = "Usuario registrado correctamente", content = @Content(schema = @Schema(implementation = UsuarioDTO.class))),
-    @ApiResponse(responseCode = "400", description = "Datos inválidos para el registro", content = @Content),
-    @ApiResponse(responseCode = "500", description = "Error al registrar el usuario", content = @Content)
-})
+        @ApiResponse(responseCode = "201", description = "Usuario registrado correctamente", content = @Content(schema = @Schema(implementation = UsuarioDTO.class))),
+        @ApiResponse(responseCode = "400", description = "Datos inválidos para el registro", content = @Content),
+        @ApiResponse(responseCode = "500", description = "Error al registrar el usuario", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<?> registrarUsuario(@Valid @RequestBody UsuarioDTO usuario) {
         try {
             UsuarioDTO nuevoRegistro = registroService.registrarUsuario(usuario);
-            return ResponseEntity.status(HttpStatus.CREATED).body(nuevoRegistro);
+
+            EntityModel<UsuarioDTO> recurso = EntityModel.of(nuevoRegistro);
+
+            
+            recurso.add(linkTo(methodOn(RegistroController.class).registrarUsuario(usuario)).withSelfRel());
+            recurso.add(linkTo(methodOn(RegistroController.class).registrarUsuario(usuario)).withRel("registrar"));
+            recurso.add(Link.of("http://localhost:8081/api/login").withRel("login"));
+
+            
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(recurso);
+
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Error al registrar: " + e.getMessage());
